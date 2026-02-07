1e1f28f2ccb435c1b34cc5c2e82768e8da7d01f5
SokLeangSiv <lsok@paragoniu.edu.kh>
2026-02-07 14:12:24 +0700

    testing5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import { rule } from "postcss";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  {rules: {
-    "no-unused-vars": "error",
-  }},
+  {
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-    "node_modules/**",
-    "dist/**",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'node_modules/**',
+    'dist/**',
   ]),
 ]);
 
